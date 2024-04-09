82c6551cb1e0771f77f540d7dfa9309448bfb535
dtp06077 <dtp06077@gmail.com>
2024-04-09 22:48:36 +0900

    Feat: 회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor //생성자 자동 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    //중복 회원 검증 메서드
+    private void validateDuplicateMember(Member member) {
+        //이름으로 중복 회원을 검증하면 동명이인은 어떤 로직으로 처리할지
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
