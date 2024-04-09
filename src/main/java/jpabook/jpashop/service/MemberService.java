5d2c223391470895048657343fd21f1b67b67427
dtp06077 <dtp06077@gmail.com>
2024-04-09 23:02:34 +0900

    Fix: Transactional 변경

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,16 +1,15 @@
 package jpabook.jpashop.service;
 
-import jakarta.transaction.Transactional;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor //생성자 자동 생성
 public class MemberService {
 
