b22acd2f7701fe36463bb5c649d2a8bd53634f95
dtp06077 <dtp06077@gmail.com>
2024-04-11 21:36:04 +0900

    Test: 주문 취소 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,7 @@ class OrderServiceTest {
     @Autowired OrderRepository orderRepository;
 
     @Test
+    @DisplayName("상품 주문")
     public void 상품주문() {
         //Given
         Member member = createMember();
@@ -42,14 +44,36 @@ class OrderServiceTest {
         //Then
         Order getOrder = orderRepository.findOne(orderId);
 
-        //상품 주문시 상태는 ORDER//
+        //==상품 주문시 상태는 ORDER==//
         assertEquals(OrderStatus.ORDER, getOrder.getStatus());
-        //주문한 상품 종류 수가 정확해야 한다//
+        //==주문한 상품 종류 수가 정확해야 한다==//
         assertEquals(1, getOrder.getOrderItems().size());
-        //주문 가격은 가격*수량//
+        //==주문 가격은 가격*수량==//
         assertEquals(orderCount*item.getPrice(), getOrder.getTotalPrice());
-        //주문 수량만큼 재고가 줄어야 한다//
+        //==주문 수량만큼 재고가 줄어야 한다==//
         assertEquals(8, item.getStockQuantity());
+    }
+
+    @Test
+    @DisplayName("주문 취소")
+    public void 주문취소() {
+        //Given
+        Member member = createMember();
+        Item item = createBook("spring JPA", 20000, 10);
+        int orderCount = 4;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //When
+        orderService.cancelOrder(orderId);
+
+        //Then
+        //주문을 취소해도 주문 엔티티는 남아있음
+        Order order = orderRepository.findOne(orderId);
+        //==주문 취소 시 상태는 CANCEL 이다==//
+        assertEquals(OrderStatus.CANCEL, order.getStatus());
+        //==주문이 취소된 상품은 그만큼 재고가 증가해야 한다==//
+        assertEquals(10,item.getStockQuantity());
     }
     //==중복되는 회원, 아이템 생성 코드 만들어놓기==//
     private Member createMember() {
