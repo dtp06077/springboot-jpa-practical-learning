188118ff2c7fab05370d748bfa5a154a5029c2cd
dtp06077 <dtp06077@gmail.com>
2024-06-19 17:04:41 +0900

    Feat: 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -46,12 +49,65 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 회원 수정
+     */
     @PatchMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
                                                @RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
         Member member = memberService.findOne(id);
         return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+    /**
+     *
+     조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     *
+     문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     *   - 기본적으로 엔티티의 모든 값이 노출된다.
+     *   - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     *   - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스 생성으
+     로 해결)
+     *
+     결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+    @Data
+    static class Result<T> {
+        private int count;
+        private T data;
+
+        public Result(int count, T data) {
+            this.count = count;
+            this.data = data;
+        }
+    }
+
+    @Data
+    static class MemberDto {
+        private String name;
+
+        public MemberDto(String name) {
+            this.name = name;
+        }
     }
 
     @Data
