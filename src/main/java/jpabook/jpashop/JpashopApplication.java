4f832755415383f323951b87d986fe27a2532840
dtp06077 <dtp06077@gmail.com>
2024-06-20 02:01:20 +0900

    Feat: 주문 조회 API V1 : 엔티티를 직접 노출

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,10 +1,22 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
+
+
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule module = new Hibernate5JakartaModule();
+		//강제 지연 로딩 설정 -> 대신 LAZY 강제 초기화 사용
+		//module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+
+		return module;
+	}
 
 	public static void main(String[] args) {
 
